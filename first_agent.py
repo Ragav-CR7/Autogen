4e1087e23520ae40e43df7ad5374b5efeff70e3c
Ragavaa-M <mragavaa@ceiamerica.com>
2025-03-27 19:04:18 +0530

    Completed First Agent

---
+++ first_agent.py
@@ -0,0 +1,36 @@
+from autogen_agentchat.agents import AssistantAgent
+from autogen_agentchat.messages import TextMessage
+from autogen_agentchat.ui import Console
+from autogen_core import CancellationToken
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+import asyncio
+import yaml 
+
+# Define a tool that searches the web for information.
+async def web_search(query: str) -> str:
+    """Find information on the web"""
+    return "AutoGen is a programming framework for building multi-agent applications."
+
+with open("config.yaml", "r") as file:
+    config = yaml.safe_load(file)
+
+model_client = OpenAIChatCompletionClient(**config["model_config"])
+
+agent = AssistantAgent(
+    name="assistant",
+    model_client=model_client,
+    tools=[web_search],
+    system_message="Use tools to solve tasks.",
+)
+
+async def assistant_run() -> None:
+    response = await agent.on_messages(
+        [TextMessage(content="Find information on AutoGen", source="user")],
+        cancellation_token=CancellationToken(),
+    )
+    print(response.inner_messages)
+    print(response.chat_message)
+
+
+# Use asyncio.run(assistant_run()) when running in a script.
+asyncio.run(assistant_run())
